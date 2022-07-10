7e7ce4d80627996abe4a214cf34f83fb70c4ab2d
Rafael Cavalcante <rvmascavalcante@gmail.com>
2022-07-09 22:23:43 -0300

    feat: ensure SignUpController returns 400 if password does not match

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,8 +1,28 @@
 export class SignUpController {
   perform (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing params: name')
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing params: name')
+      }
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing params: email')
+      }
+    }
+    if (!httpRequest.body.password) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing params: password')
+      }
+    }
+    if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      return {
+        statusCode: 400,
+        body: new Error('Invalid params: passwordConfirmation')
+      }
     }
   }
 }
