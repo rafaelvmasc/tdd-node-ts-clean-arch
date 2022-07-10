d940dc6603056f35a542b62dbe1cef64984d488b
Rafael Cavalcante <rvmascavalcante@gmail.com>
2022-07-10 14:55:37 -0300

    test: ensure SignUpController returns 200 on success

--- src/presentation/controllers/signup-controller.spec.ts
+++ src/presentation/controllers/signup-controller.spec.ts
@@ -197,4 +197,24 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password',
+        email: 'valid_email@mail.com'
+      }
+    }
+    const httpResponse = await sut.perform(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      password: 'valid_password',
+      email: 'valid_email@mail.com'
+    })
+  })
 })
