1839bdc7c7bbebb649fc437f9a089edc628e27fd
Rafael Cavalcante <rvmascavalcante@gmail.com>
2022-07-09 23:42:47 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup-controller.ts
+++ src/presentation/controllers/signup-controller.ts
@@ -1,5 +1,5 @@
-import { InvalidParamError, MissingParamError } from '../errors'
-import { badRequest, HttpRequest, HttpResponse } from '../helpers'
+import { InvalidParamError, MissingParamError, ServerError } from '../errors'
+import { badRequest, HttpRequest, HttpResponse, serverError } from '../helpers'
 import { EmailValidator } from '../protocols'
 import { Controller } from '../protocols/controller'
 
@@ -9,23 +9,27 @@ export class SignUpController implements Controller {
   ) {}
 
   async perform (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const requiredField = ['name', 'email', 'password', 'passwordConfirmation']
-    for (const field of requiredField) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+    try {
+      const requiredField = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requiredField) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
-    if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return {
+          statusCode: 400,
+          body: new InvalidParamError('passwordConfirmation')
+        }
+      }
+      const isEmailValid = await this.emailValidator.isValid(httpRequest.body.email)
+      if (!isEmailValid) return badRequest(new InvalidParamError('email'))
       return {
-        statusCode: 400,
-        body: new InvalidParamError('passwordConfirmation')
+        statusCode: 200,
+        body: ''
       }
-    }
-    const isEmailValid = await this.emailValidator.isValid(httpRequest.body.email)
-    if (!isEmailValid) return badRequest(new InvalidParamError('email'))
-    return {
-      statusCode: 200,
-      body: ''
+    } catch (error) {
+      return serverError(new ServerError())
     }
   }
 }
