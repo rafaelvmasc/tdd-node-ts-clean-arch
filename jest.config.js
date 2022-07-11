178bc8041434c81a780f8d8a08f5462e80005b6d
Rafael Cavalcante <rvmascavalcante@gmail.com>
2022-07-11 17:15:06 -0300

    feat: add mongodb in memory

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageProvider: 'v8',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
---
+++ src/infra/database/mongodb/account-repository.spec.ts
@@ -0,0 +1,38 @@
+import { AccountRepository } from './account-repository'
+import { MongoHelper } from './helpers/mongo-helper'
+
+interface SutTypes {
+  sut: AccountRepository
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new AccountRepository()
+  return {
+    sut
+  }
+}
+
+describe('Account MongoDB Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL ?? '')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password'
+    }
+    const accountCreated = await sut.add(accountData)
+    expect(accountCreated).toBeTruthy() // significa que nao é nulo/undefined
+    expect(accountCreated.id).toBeTruthy()
+    expect(accountCreated.name).toBe('any_name')
+    expect(accountCreated.email).toBe('any_email')
+    expect(accountCreated.password).toBe('any_password')
+  })
+})
