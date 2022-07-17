c8d9a97768760826d165ef7194b61cc811242310
Rafael Cavalcante <rvmascavalcante@gmail.com>
2022-07-17 12:05:52 -0300

    test: ensure RequiredFieldValidation returns an error if validation fails

---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,24 @@
+import { MissingParamError } from '../../errors'
+import { RequiredFieldValidation } from './required-field-validation'
+
+describe('Required Field Validation', () => {
+  test('Should return MissingParamError if a required field is not provided', () => {
+    const fieldName = 'field'
+    const sut = new RequiredFieldValidation(fieldName)
+    const input = {
+      email: 'any_email@mail.com'
+    }
+    const error = sut.validate(input)
+    expect(error).toEqual(new MissingParamError(fieldName))
+  })
+
+  test('Should return null if validation is ok', () => {
+    const fieldName = 'field'
+    const sut = new RequiredFieldValidation(fieldName)
+    const input = {
+      field: 'any_value'
+    }
+    const error = sut.validate(input)
+    expect(error).toBeFalsy()
+  })
+})
