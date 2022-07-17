4dc5cfd4d24aff00b405ae3166c55f6c6aeba963
Rafael Cavalcante <rvmascavalcante@gmail.com>
2022-07-16 23:50:33 -0300

    feat: add LogMongoRepository to signup controller factory

--- src/main/factories/controllers/signup-controller-factory.ts
+++ src/main/factories/controllers/signup-controller-factory.ts
@@ -1,3 +1,4 @@
+import { LogMongoRepository } from '../../../infra/database/mongodb/log-repository/log'
 import { SignUpController } from '../../../presentation/controllers/signup-controller/signup-controller'
 import { Controller } from '../../../presentation/protocols/controller'
 import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
@@ -7,6 +8,7 @@ import { makeAddAccountService } from '../usecases/add-account'
 export const makeSignUpController = (): Controller => {
   const addAccountService = makeAddAccountService()
   const emailValidator = new EmailValidatorAdapter()
+  const logRepository = new LogMongoRepository()
   const signUpController = new SignUpController(addAccountService, emailValidator)
-  return new LogControllerDecorator(signUpController)
+  return new LogControllerDecorator(signUpController, logRepository)
 }
