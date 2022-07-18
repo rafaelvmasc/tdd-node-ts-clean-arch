837f52517ab0a28bfa14c3643798d564b1e5e9c7
Rafael Cavalcante <rvmascavalcante@gmail.com>
2022-07-18 10:30:15 -0300

    test: ensure login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -51,4 +51,14 @@ describe('Login Routes', () => {
         .expect(200)
     })
   })
+
+  test('Should return 401 on login', async () => {
+    await request(app)
+      .post('/api/login')
+      .send({
+        email: 'rafa@mail.com',
+        password: 'invalid_password'
+      })
+      .expect(401)
+  })
 })
