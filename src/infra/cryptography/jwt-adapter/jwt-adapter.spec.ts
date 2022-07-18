b65fbcf132fc089df453b9a4a80cdce860ec12b1
Rafael Cavalcante <rvmascavalcante@gmail.com>
2022-07-17 23:57:24 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -31,4 +31,11 @@ describe('Jwt Adapter', () => {
     const token = await sut.encrypt('any_id')
     expect(token).toBe('jwt_token')
   })
+
+  test('Should throw if sign throws', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_id')
+    expect(promise).rejects.toThrow()
+  })
 })
